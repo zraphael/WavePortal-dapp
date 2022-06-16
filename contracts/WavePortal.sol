a7b3c565b082c2004053f383f8211d1d9e1070b9
zraphael <raphael@wcarvalho.com>
2022-06-16 18:26:15 -0300

    feat: final contract with uitility scripts

--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -6,17 +6,59 @@ import "hardhat/console.sol";
 
 contract WavePortal {
     uint256 totalWaves;
-    mapping (address => uint) wavers;
+    uint256 seed;
 
-    constructor() {
-        console.log("Yeee, I'm a contract and I'm damn smart!");
+    event NewWave(address indexed from, uint256 timestamp, string message);
+
+    struct Wave {
+        address waver;
+        string message;
+        uint256 timestamp;
     }
 
-    function wave() public {
+    Wave[] waves;
+
+    mapping(address => uint) wavers;
+    mapping(address => uint256) public lastWavedAt;
+
+    constructor() payable {
+        console.log("Yeee, I'm a contract and I'm damn smart!");
+        seed = (block.timestamp + block.difficulty) % 100;
+    }
+
+    function wave(string memory _message) public {
+        require(
+            lastWavedAt[msg.sender] + 1 seconds < block.timestamp,
+            "Wait 30 seconds before sending another wave."
+        );
+
+        lastWavedAt[msg.sender] = block.timestamp;
+
         totalWaves += 1;
         wavers[msg.sender] = wavers[msg.sender] + 1;
-        console.log("%s sent you a wave!", msg.sender);
-        console.log("%s already sent you %d waves", msg.sender, wavers[msg.sender]);
+        console.log("%s waved with the following message: %s", msg.sender, _message);
+
+        waves.push(Wave(msg.sender, _message, block.timestamp));
+
+        seed = (block.difficulty + block.timestamp + seed) % 100;
+
+        if(seed <= 50) {
+            console.log("%s won!", msg.sender);
+
+            uint256 prizeAmount = 0.0001 ether;
+            require(
+                prizeAmount <= address(this).balance,
+                "Trying to withdraw more money than the contract actually have."
+            );
+            (bool success, ) = (msg.sender).call{value: prizeAmount}("");
+            require(success, "Failed in withdrawing money from the contract.");
+        }
+
+        emit NewWave(msg.sender, block.timestamp, _message);
+    }
+
+    function getAllWaves() public view returns (Wave[] memory) {
+        return waves;
     }
 
     function getTotalWaves() public view returns (uint256) {
--- scripts/run.js
+++ scripts/run.js
@@ -1,34 +1,43 @@
 const main = async () => {
-    const [owner, randomPerson] = await hre.ethers.getSigners();
     const waveContractFactory = await hre.ethers.getContractFactory("WavePortal");
-    const waveContract = await waveContractFactory.deploy();
+    const waveContract = await waveContractFactory.deploy({
+      value: hre.ethers.utils.parseEther("0.1"),
+    });
     await waveContract.deployed();
-
-    console.log("Contract deployed to:", waveContract.address);
-    console.log("Contract deployed by:", owner.address);
-
-    let waveCount;
-    waveCount = await waveContract.getTotalWaves();
-
-    let waveTxn = await waveContract.wave();
+    console.log("Contract Address:", waveContract.address);
+  
+    let contractBalance = await hre.ethers.provider.getBalance(
+      waveContract.address
+    );
+    console.log(
+      "Address balance:",
+      hre.ethers.utils.formatEther(contractBalance)
+    );
+  
+    let waveTxn = await waveContract.wave("A message!");
     await waveTxn.wait();
 
-    waveCount = await waveContract.getTotalWaves();
-
-    waveTxn = await waveContract.connect(randomPerson).wave();
-    await waveTxn.wait();
-
-    waveCount = await waveContract.getTotalWaves();
-};
-
-const runMain = async () => {
+    let waveTxn2 = await waveContract.wave("A second message!");
+    await waveTxn2.wait();
+  
+    contractBalance = await hre.ethers.provider.getBalance(waveContract.address);
+    console.log(
+      "Contract balance:",
+      hre.ethers.utils.formatEther(contractBalance)
+    );
+  
+    let allWaves = await waveContract.getAllWaves();
+    console.log(allWaves);
+  };
+  
+  const runMain = async () => {
     try {
-        await main();
-        process.exit(0);
+      await main();
+      process.exit(0);
     } catch (error) {
-        console.log(error);
-        process.exit(1);
+      console.log(error);
+      process.exit(1);
     }
-};
-
-runMain();
+  };
+  
+  runMain();
